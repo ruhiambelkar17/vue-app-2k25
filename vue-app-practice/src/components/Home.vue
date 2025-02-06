caa2cbf35582852653979fdb92efaf91693cee94
ruhiambelkar17 <ruhiambelkar17@gmail.com>
2025-02-06 12:00:39 +0530

    craeted vue app

---
+++ vue-app-practice/src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div>Welcome!!</div>
+        <h1>I'm {{ name }}</h1>
+    </div>
+</template>
+
+<script>
+
+export default{
+    data(){
+        return {
+            name:""
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+
+</style>
