a5af87c7220d351cfab7c4994a2577334f58ad2b
ruhiambelkar17 <ruhiambelkar17@gmail.com>
2025-02-07 00:02:22 +0530

    added vuex

--- vue-app-practice/src/main.js
+++ vue-app-practice/src/main.js
@@ -2,14 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import FormComp from './components/FormComp.vue'
 import router from './router/router'
-import axios from 'axios'
+
 
 
 const app=createApp(App);
 
 
 app.use(router);
-app.use(axios)
+
 
 app.component("FormComp",FormComp);
 
